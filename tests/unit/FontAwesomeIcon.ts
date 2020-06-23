2d6c5b188692a3ec90c88cfe51fe28b58e4750d7
xiaohulu <idocument@qq.com>
2020-06-23 14:52:35 +0800

    build: upgrade dojo to v7.0

--- tests/unit/FontAwesomeIcon.ts
+++ tests/unit/FontAwesomeIcon.ts
@@ -1,7 +1,7 @@
 const { describe, it } = intern.getInterface("bdd");
 
-import assertionTemplate from "@dojo/framework/testing/assertionTemplate";
-import harness from "@dojo/framework/testing/harness";
+import assertionTemplate from "@dojo/framework/testing/harness/assertionTemplate";
+import harness from "@dojo/framework/testing/harness/harness";
 import { w, v } from "@dojo/framework/core/vdom";
 
 import FontAwesomeIcon from "../../src/FontAwesomeIcon";
